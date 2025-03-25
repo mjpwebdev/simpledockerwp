a719088cf04b247141d2f8683d5f71f54cd41657
mjpwebdev <150134291+mjpwebdev@users.noreply.github.com>
2025-03-25 14:49:02 +0000

    Update quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -6,18 +6,33 @@ prompt_input() {
   local default_value="$3"
   read -p "$prompt_message [$default_value]: " input_value
   input_value="${input_value:-$default_value}"
+  export "$var_name"="$input_value" 
   echo "$var_name=$input_value" >> .env
 }
+
 configure_wp_config() {
   local db_name="$1"
   local db_user="$2"
   local db_password="$3"
-  local db_host="$4"
+  local db_port="$4"
   cp wordpress/wp-config-sample.php wordpress/wp-config.php
-  sed -i "" "s/database_name_here/$db_name/" wordpress/wp-config.php
-  sed -i "" "s/username_here/$db_user/" wordpress/wp-config.php
-  sed -i "" "s/password_here/$db_password/" wordpress/wp-config.php
-  sed -i "" "s/127.0.0.1/$db_host/" wordpress/wp-config.php
+  sed_in_place "database_name_here" "$db_name" "wordpress/wp-config.php"
+  sed_in_place "username_here" "$db_user" "wordpress/wp-config.php"
+  sed_in_place "password_here" "$db_password" "wordpress/wp-config.php"
+  sed_in_place "localhost" "127.0.0.1:$db_port" "wordpress/wp-config.php"
+}
+
+sed_in_place() {
+  local pattern="$1"
+  local replacement="$2"
+  local file="$3"
+  if [[ "$(uname)" == "Darwin" ]]; then
+    # macOS
+    sed -i "" "s/$pattern/$replacement/" "$file"
+  else
+    # Linux
+    sed -i "s/$pattern/$replacement/" "$file"
+  fi
 }
 > .env
 prompt_input "MYSQL_DATABASE" "Enter MySQL database name" "db"
@@ -25,9 +40,8 @@ prompt_input "MYSQL_USER" "Enter MySQL username" "user"
 prompt_input "MYSQL_PASSWORD" "Enter MySQL password" "password"
 prompt_input "MYSQL_ROOT_PASSWORD" "Enter MySQL root password" "password"
 prompt_input "MYSQL_TCP_PORT" "Enter MySQL TCP port" "3308"
-configure_wp_config "$MYSQL_DATABASE" "$MYSQL_USER" "$MYSQL_PASSWORD" "db:$MYSQL_TCP_PORT"
-echo "Generated .env file:"
 cat .env
 docker compose up -d
-sleep 5
+sleep 5  
+configure_wp_config "$MYSQL_DATABASE" "$MYSQL_USER" "$MYSQL_PASSWORD" "$MYSQL_TCP_PORT"
 php -S localhost:8082 -t wordpress
